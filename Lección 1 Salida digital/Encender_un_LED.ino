c0b78456ab47f7b6b53466b6dbfb04cc6881798d
Ernesto Leal <ernesto.leal@uelectronics.com>
2024-07-23 16:56:38 -0600

    Primer programa de encender led

---
+++ Lección 1 Salida digital/Encender_un_LED.ino
@@ -0,0 +1,14 @@
+void setup() {
+  //Coloque el código de configuracón aquí,
+  //para que se ejecute una vez:
+
+  pinMode(13, OUTPUT); 
+
+}
+
+void loop() {
+ //Coloque el código principal aquí,
+ // para que se ejecute repetidamente:
+
+ digitalWrite(13, HIGH);
+}
