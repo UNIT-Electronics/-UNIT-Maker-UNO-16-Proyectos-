c4d652432a7466684088b6a473b6903481e64e9c
Ernesto Leal <ernesto.leal@uelectronics.com>
2024-07-23 17:04:30 -0600

    Segundo programa prender y apagar un led

---
+++ Lección 1 Salida digital/Parpadeo_de_un_LED.ino
@@ -0,0 +1,17 @@
+void setup() {
+  //Coloque el código de configuracón aquí,
+  //para que se ejecute una vez:
+
+  pinMode(13, OUTPUT); 
+
+}
+
+void loop() {
+ //Coloque el código principal aquí,
+ // para que se ejecute repetidamente:
+
+ digitalWrite(13, HIGH);
+ delay(100);
+ digitalWrite(13, LOW);
+ delay(100);
+}
