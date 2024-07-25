0c122868fd9f292b9e63c9cf5af3704fa8b00ed9
Ernesto Leal <ernesto.leal@uelectronics.com>
2024-07-25 12:58:03 -0600

    Conexiones para el proyecto 7 de la Lección 4

---
+++ Lección 4 Tonos de melodía/Proyecto_7.ino
@@ -0,0 +1,28 @@
+/*
+  Derechos de autor © UNIT Electronics
+  Este codigo se proporciona "tal cual", sin garantías de ningún tipo.
+  Fecha de revisión: 25/07/24
+*/
+/********************************
+ En este proyecto se programa tonos básicos en el UNO para que
+salgan por medio de un buzzer
+ ********************************/
+void setup() {
+  pinMode (8,OUTPUT);
+}
+
+void loop() {
+  tone (8,262,250); delay (325);
+  tone (8,294, 250); delay (325);
+  tone (8,330, 250); delay (325);
+  tone (8,349, 250);
+  delay (325);
+  tone (8,392, 250);
+  delay (325);
+  tone (8,440, 250);
+  delay (325);
+  tone (8,494, 250);
+  delay (325);
+  tone (8,523, 250); delay(1000);
+  delay(1000);
+}
