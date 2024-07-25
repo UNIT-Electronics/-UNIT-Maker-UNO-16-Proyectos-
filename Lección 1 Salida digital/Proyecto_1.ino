a7e15dacdf2287abe175a9f17fa2da0621d4e5da
Ernesto Leal <ernesto.leal@uelectronics.com>
2024-07-25 11:49:54 -0600

    Este es el proyecto 1 : Encender un LED

--- Lección 1 Salida digital/Proyecto_1.ino
+++ Lección 1 Salida digital/Proyecto_1.ino
@@ -1,3 +1,11 @@
+/*
+  Derechos de autor © UNIT Electronics
+  Este codigo se proporciona "tal cual", sin garantías de ningún tipo.
+  Fecha de revisión: 25/07/24
+*/
+/********************************
+ Encendido de un LED  
+ ********************************/
 void setup() {
   //Coloque el código de configuracón aquí,
   //para que se ejecute una vez:
